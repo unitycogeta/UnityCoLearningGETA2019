d33b17b83e84d7df4dda17bab519ad59999ee8b7
IvanM <34187246+IvanM@users.noreply.github.com>
2019-07-07 00:19:10 +0200

    EjectedSandpiles, Checkpoints, See-saws, gamecontroller implemented

---
+++ UnityCoLearningGETA2019/Assets/_IvanWorkFolder/EjectedSand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EjectedSand : MonoBehaviour
+{
+    internal bool isAbsorable;
+
+    private void Start()
+    {
+        StartCoroutine("WaitAndPrint",1);
+    }
+
+    private IEnumerator WaitAndPrint(float waittime)
+    {
+        yield return new WaitForSeconds(waittime);
+        isAbsorable = true;
+    }
+
+    internal void Absorb()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ UnityCoLearningGETA2019/Assets/_IvanWorkFolder/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public int maxEjectedSand = 10;
+    private Sandpile[] allSandpiles;
+    internal List<EjectedSand> allEjectedSand;
+    private MovableElement[] allMovables;
+
+    private void Start()
+    {
+        allSandpiles = FindObjectsOfType<Sandpile>();
+        allMovables = FindObjectsOfType<MovableElement>();
+        allEjectedSand = new List<EjectedSand>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("FullReset"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void AddEjectedSand(EjectedSand eject)
+    {
+        allEjectedSand.Add(eject);
+
+        if(allEjectedSand.Count > maxEjectedSand)
+        {
+            allEjectedSand.RemoveRange(0, allEjectedSand.Count - maxEjectedSand);
+        }
+        Debug.Log(allEjectedSand.Count);
+        
+    }
+
+    public void PerformCheckpointReset()
+    {
+        foreach(var sandpile in allSandpiles)
+        {
+            sandpile.Reset();
+        }
+        foreach(var ejectedSand in allEjectedSand.ToArray())
+        {
+            allEjectedSand.Remove(ejectedSand);
+            ejectedSand.Absorb();
+        }
+        foreach(var movable in allMovables)
+        {
+            movable.ResetToInitial();
+        }
+    }
+
+}
